f95031568de99b8d659fae137462f0e091620e20
Star <101126792+Starx707@users.noreply.github.com>
2025-09-09 14:59:53 +0200

    Style and layout of 'City Garden' page

---
+++ include/pages/city_garden.php
@@ -0,0 +1,67 @@
+<?php
+//CamelCase = ID
+//- = Class
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our City Garden</title>
+    <script></script>
+    <link rel="stylesheet" href="/include/css/style.css">
+</head>
+<header id="headerCG">
+    <a href="/index.php"><span><</span></a>
+    <p>Our Garden City</p>
+</header>
+<body>
+<!--menu-->
+        <div id="pageNavigationCityGarden">
+            <a href="#Quiz">Quiz</a>
+            <a href="#Map">Map</a>
+            <a href="#News">News</a>
+        </div>
+    <section id="Quiz">
+        <div>
+            <!--Quiz-->
+            <div id="quiz-div">
+
+            </div>
+        </div>
+    </section>
+    <section>
+        <div id="Map">
+            <!--Map-->
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19683.55630586389!2d4.489215999999999!3d51.925844649999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c4335f7169b77d%3A0x1c6f77a369702fe0!2sBoekhandel%20Donner!5e0!3m2!1snl!2snl!4v1757418736673!5m2!1snl!2snl" width="100%" height="450" style="border: 2px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div id="Answers">
+            <!--Answer request-->
+            <p></p>
+            <div>
+                <div>
+                    <!--image-->
+
+                    <!--text-->
+                </div>
+                <div>
+                    <!--image-->
+                    <!--text-->
+                </div>
+                <div>
+                    <!--image-->
+                    <!--text-->
+                </div>
+            </div>
+            <button>Kies plek</button>
+            <span></span>
+        </div>
+    </section>
+    <section id="News">
+
+    </section>
+</body>
+</html>
